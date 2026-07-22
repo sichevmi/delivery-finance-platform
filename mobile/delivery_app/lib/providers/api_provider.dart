8f7b2b32fb97f4481ba0b55e089b1c73f0e9bf64
Макс <sichevmi@gmail.com>
2026-07-22 10:10:01 +0300

    снова поменял урл в моб приложении

--- mobile/delivery_app/lib/providers/api_provider.dart
+++ mobile/delivery_app/lib/providers/api_provider.dart
@@ -4,7 +4,7 @@ import 'package:delivery_app/services/storage_service.dart';
 
 final dioProvider = Provider<Dio>((ref) {
   final dio = Dio(BaseOptions(
-    baseUrl: 'https://тест.финфлоу.рф/api/v1', // замените на ваш IP
+    baseUrl: 'http://195.19.20.178:8001/api/v1', // замените на ваш IP
     connectTimeout: const Duration(seconds: 10),
     receiveTimeout: const Duration(seconds: 10),
   ));
@@ -27,7 +27,7 @@ final dioProvider = Provider<Dio>((ref) {
         if (refreshToken != null) {
           try {
             // Вызываем /auth/refresh
-            final dio2 = Dio(BaseOptions(baseUrl: 'http://192.168.1.121:8001/api/v1'));
+            final dio2 = Dio(BaseOptions(baseUrl: 'http://195.19.20.178:8001/api/v1'));
             final response = await dio2.post('/auth/refresh', data: {'refresh_token': refreshToken});
             final newAccess = response.data['access_token'];
             final newRefresh = response.data['refresh_token'] ?? refreshToken; // или новый refresh, если приходит
