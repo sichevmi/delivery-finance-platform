a25b03b68f7120214119b50afb0bed336ac32c82
Макс <sichevmi@gmail.com>
2026-08-04 21:47:50 +0300

    Холостой пробег

--- mobile/delivery_app/lib/features/delivery/providers/shift_provider.dart
+++ mobile/delivery_app/lib/features/delivery/providers/shift_provider.dart
@@ -1,4 +1,5 @@
 // lib/features/delivery/providers/shift_provider.dart
+import 'package:flutter/material.dart'; // <-- ДОБАВЛЕНО
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:delivery_app/features/delivery/providers/gps_provider.dart';
